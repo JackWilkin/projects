3685e347ee59e6b14949be1cb5a48a3c4d5bb7b9
Jack Wilkin <wilkin.j@husky.neu.edu>
2020-11-10 22:26:42 -0500

    Remove broken external blink component

--- src/Components/BlinkingLight.jsx
+++ src/Components/BlinkingLight.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import Blink from 'react-blink-text';
 
 const Container = styled.div`
   margin-bottom: 61px;
   text-shadow: 0px 0px 7px black;
+  width: 2rem;
 `;
+
+const Text = styled.div`
+  color: yellow;
+  font-size: 1600%;
+
+`;
+
+function BlinkingText(props) {
+  const [showText, setShowText] = React.useState(true);
+  const { text } = props;
+
+  React.useEffect(() => {
+    // Change the state every second or the time given by User.
+    const interval = setInterval(() => {
+      setShowText((state) => !state);
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  const display = showText ? text : ' ';
+  return (
+    <Text>{display}</Text>
+  );
+}
 
 export default function BlinkingLight() {
   return (
     <Container>
-      <Blink fontStyle="Helvetica" color="yellow" text="." fontSize="1600%" />
+      <BlinkingText color="yellow" text="." fontSize="1600%" />
     </Container>
 
   );
